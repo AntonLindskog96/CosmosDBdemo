a69b17eb019ae992c08d0286999a39b5181481fb
Anton Lindskog <lindskog96@live.se>
2025-02-27 09:06:43 +0100

    added, delete, getALl, getPersonById

--- src/main/java/com/example/cosmosdbapp/controller/PersonController.java
+++ src/main/java/com/example/cosmosdbapp/controller/PersonController.java
@@ -5,10 +5,10 @@ import com.example.cosmosdbapp.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/people")
@@ -21,5 +21,25 @@ public class PersonController {
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         Person savedPerson = personService.createPerson(person);
         return new ResponseEntity<>(savedPerson, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable String id) {
+        Optional<Person> person = personService.getPersonById(id);
+        return person.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable String id) {
+        boolean isDeleted = personService.deletePerson(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/example/cosmosdbapp/service/PersonService.java
+++ src/main/java/com/example/cosmosdbapp/service/PersonService.java
@@ -5,6 +5,9 @@ import com.example.cosmosdbapp.repository.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PersonService {
 
@@ -13,5 +16,18 @@ public class PersonService {
 
     public Person createPerson(Person person) {
         return personRepo.save(person);
+    }
+    public List<Person> getAllPersons() {
+        return personRepo.findAll();
+    }
+    public boolean deletePerson(String id) {
+        if (personRepo.existsById(id)) {
+            personRepo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+    public Optional<Person> getPersonById(String id) {
+        return personRepo.findById(id);
     }
 }
